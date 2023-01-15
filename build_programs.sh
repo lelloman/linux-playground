910a6eb3b5df7e13dc61bfe989ba994a1e89a959
Domenico Cerasuolo <cerasuolodomenico@gmail.com>
2023-01-15 21:43:19 +0100

    Fix /programs creation

--- build_programs.sh
+++ build_programs.sh
@@ -6,13 +6,15 @@ cargo build --release
 cd ../
 
 # copy programs
-rm programs/*
+rm -rf programs
+mkdir programs
+mkdir fs_overlay/programs
 cp nettest/target/release/nettestserver programs
 
-rm programs.img
+sudo rm programs.img
 dd if=/dev/zero of=programs.img bs=1M count=1024
 mkfs.ext2 programs.img
 sudo mount -o loop programs.img /mnt
 sudo cp -a programs/* /mnt
-chmod +x /mnt/*
+sudo chmod +x /mnt/*
 sudo umount /mnt
