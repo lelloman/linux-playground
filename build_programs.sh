922345042d60118bd9ac23f15fede7f5d5b5775b
Domenico Cerasuolo <cerasuolodomenico@gmail.com>
2023-01-20 17:30:24 +0100

    Remove unused programs

--- build_programs.sh
+++ build_programs.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
 # build programs
-cd nettest
-cargo build --release
-cd ../
-
-cd kmallocer
-cargo build --release
-cd ../
-
 cd mfiller
 cargo build --release
 cd ../
@@ -22,7 +14,5 @@ rm -rf shared/bin
 mkdir shared/bin
 
 # copy programs
-cp nettest/target/release/nettestserver shared/bin
-cp kmallocer/target/release/kmallocer shared/bin
 cp mfiller/target/release/mfiller shared/bin
 cp kmallocer-mod/kmallocer.ko shared
